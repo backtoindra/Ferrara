969b5b0d16ae76add3082df90cf9e72797a96126
bodasaieswar <bodasaieswar@gmail.com>
2024-08-24 21:51:03 +0000

    updated the timings

--- src/components/SpecialRequest.tsx
+++ src/components/SpecialRequest.tsx
@@ -19,17 +19,17 @@ const SpecialRequests = () => {
           <div className="text-center">
             <h3 className="text-xl font-bold text-gray-900 md:text-left">Hours</h3>
             <ul className="mt-4 text-lg text-gray-600 md:text-right">
-              <li className='font-semibold'>Mon 10:30 – 21:00</li>
-              <li className='font-semibold'>Tue 10:30 – 21:00</li>
-              <li className='font-semibold'>Wed 10:30 – 21:00</li>
-              <li className='font-semibold'>Thu 10:30 – 21:00</li>
-              <li className='font-semibold'>Fri 10:30 – 21:00</li>
-              <li className='font-semibold'>Sat 10:30 – 21:00</li>
+              <li className='font-semibold'>Mon 10:30 – 21:30</li>
+              <li className='font-semibold'>Tue 10:30 – 21:30</li>
+              <li className='font-semibold'>Wed 10:30 – 21:30</li>
+              <li className='font-semibold'>Thu 10:30 – 21:30</li>
+              <li className='font-semibold'>Fri 10:30 – 22:30</li>
+              <li className='font-semibold'>Sat 10:30 – 22:30</li>
               <li className='font-semibold'>Sun 11:00 – 21:00</li>
             </ul>
           </div>
         </div>
-      </
+      </div>
     </div>
   );
 };
